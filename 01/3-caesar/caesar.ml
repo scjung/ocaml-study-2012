a4b8b85ffb8022ca3c8ee8071c1e798f46e2ab6a
Seung Cheol Jung <scjung.hyu@gmail.com>
2012-10-16 01:12:11 +0900

    Fix the abnormal shift operation on characters.

--- 01/3-caesar/caesar.ml
+++ 01/3-caesar/caesar.ml
@@ -1,7 +1,12 @@
 let is_alpha c =
   ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z')
 
-(* 밀어낸 문자 코드가 알파벳 코드가 되도록 돌린다.
+let first_of is_upper = Char.code (if is_upper then 'A' else 'a')
+
+let last_of is_upper = Char.code (if is_upper then 'Z' else 'z')
+
+(* 문자를 정해진 수마큼 밀어낸다. 밀어낼 때는 알파벳 범위를 넘어가는 경우를
+   주의깊게 고려해야 한다.
 
    예를 들어 'y'의 코드 121을 오른쪽으로 5 밀어낸 값  126은 다음과 같이 대문자
    코드 영역으로 돌아간다.
@@ -12,19 +17,25 @@ let is_alpha c =
        |                           |
        ^---------------------------'
  *)
-let round code =
-  let code_A = Char.code 'A' in
-  let code_Z = Char.code 'Z' in
-  let code_a = Char.code 'a' in
-  let code_z = Char.code 'z' in
-  if code < code_A then
-    code_z - (code_A - code) + 1
-  else if code_Z < code && code < code_a then
-    code_a + (code - code_Z) - 1
-  else if code_z < code then
-    code_A + (code - code_z) - 1
-  else
-    code
+let shift_char n c =
+  let code = Char.code c in
+  let upper = 'A' <= c && c <= 'Z' in
+  let idx = code - first_of upper in
+  let shifted_code =
+    if n > 0 then
+      let overflow = n - (26 - idx - 1) in
+      if overflow > 0 then
+        first_of (not upper) + overflow - 1
+      else
+        code + n
+    else
+      let underflow = idx + n in
+      if underflow < 0 then
+        last_of (not upper) + underflow + 1
+      else
+        code + n
+  in
+  Char.chr shifted_code
 
 (* 주어진 문자열 내 각 알파벳을 주어진 n 만큼 밀어낸다. n 이 양수인 경우는
    오른쪽으로, 음수인 경우는 왼쪽으로 민다. 즉 shift 1 "B" 의 결과는 "C",
@@ -37,7 +48,7 @@ let shift n s =
       let plain = s.[i] in
       let encrypted =
         if is_alpha plain then
-          Char.chr (round (Char.code plain + n))
+          shift_char n plain
         else
           plain
       in
